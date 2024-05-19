573f60d3b47d4d7e8a84f8a42f2b1baa45ba075b
wsgestor <wsgestor@gmail.com>
2024-05-19 02:48:30 +0000

    cambios en pruebas idx.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -21,32 +21,17 @@
     # Enable previews
     previews = {
       enable = true;
-      previews = {
-        command = [
-          "npm"
-          "run"
-          "start"
-          "--"
-          "--port"
-          "$PORT"
-          "--host"
-          "0.0.0.0"
-          "--disable-host-check"
-        ];
-        manager = "web";
-        id = "web";
-      };
-
-      # web = {
-      #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-      #   # and show it in IDX's web preview panel
-      #   command = ["npm" "run" "dev"];
-      #   manager = "web";
-      #   env = {
-      #     # Environment variables to set for your server
-      #     PORT = "$PORT";
-      #   };
-      # };
+     
+      #web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #env = {
+          #     # Environment variables to set for your server
+          #PORT = "8000";
+        #};
+      #};
 
     };
     # Workspace lifecycle hooks
